24a85feb9fe2342698840605ff1530a94919d78d
decole2014@yandex.ru <decole>
2019-03-10 08:14:37 +0400

    fix nodemcu logic

--- NODEMCU_RELAY_TEST/NODEMCU_RELAY_TEST.ino
+++ NODEMCU_RELAY_TEST/NODEMCU_RELAY_TEST.ino
@@ -1,7 +1,15 @@
 /*
 Автор: Сергей Галочкин
 email: decole@rambler.ru
-Данный скетч для NodeMCU.
+Данный скетч для NodeMCU (Arduino IDE)
+4 электроклапана работают через 4 реле
+
+1 реле - включает блок питания и главный клапан, без этого включенного топика ни один из электроклапанов полива не будет работать (ИП отключен)
+2 реле - включает клапан полива 1
+3 реле - включает клапан полива 2
+4 реле - включает клапан полива 3
+
+При топике water/alarm отключаются все клапаны
 */
 
 #include <ESP8266WiFi.h>
@@ -11,29 +19,24 @@ int Relay1 = 16;// D0
 int Relay2 = 5; // D1
 int Relay3 = 4; // D2
 int Relay4 = 2; // D4
-int Relay5 = 14;// D5
 int Woter1 = 12;// D6
 
 // Update these with values suitable for your network.
-
 const char* ssid = "DECOLE-WIFI";
 const char* password = "A9061706210";
 const char* mqtt_server = "192.168.1.5";
+long lastMsg = 0;
+char msg[50];
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
-int value = 0;
+
 
 void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
+  delay(10);  
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -47,28 +50,15 @@ void setup_wifi() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {  
-    // In order to republish this payload, a copy must be made
-  // as the orignal payload buffer will be overwritten whilst
-  // constructing the PUBLISH packet.
-
   // Allocate the correct amount of memory for the payload copy
   byte* p = (byte*)malloc(length);
   // Copy the payload to the new buffer
   memcpy(p,payload,length);
   
  // в топиках передавать 1 или 0
-  if(String(topic).indexOf("water/trans")>= 0){
-    if(p[0] == '1') {
-      digitalWrite(Relay5, HIGH);     
-    }
-    else if(p[0] == '0') {
-      digitalWrite(Relay5, LOW);
-    }
-  }
   if(String(topic).indexOf("water/major")>= 0){
     if(p[0] == '0') {
       digitalWrite(Relay1, HIGH);    
@@ -103,57 +93,57 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   if(String(topic).indexOf("water/alarm")>= 0){
     if(p[0] == '1') {
-      digitalWrite(Relay1, LOW);
-      digitalWrite(Relay2, LOW);
-      digitalWrite(Relay3, LOW);
-      digitalWrite(Relay4, LOW);
-      digitalWrite(Relay5, LOW);     
+      // оключаем все реле
+      digitalWrite(Relay1, HIGH); 
+      digitalWrite(Relay2, HIGH);
+      digitalWrite(Relay3, HIGH);
+      digitalWrite(Relay4, HIGH); 
     }
   }
   free(p);
 }
 
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
+    // Создаем рандомный client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
-    // Attempt to connect
+    // Пробуем подключиться
     if (client.connect(clientId.c_str()), "esp", "esp99669966q") {
       Serial.println("connected");
-      // Once connected, publish an announcement...
+      // После конекта отправка тестового сообщения и подписка на топики
       client.publish("outTopic", "hello world");
-      // ... and resubscribe
+      // ... и подписываемся
       client.subscribe("water/major");
       client.subscribe("water/1");
       client.subscribe("water/2");
       client.subscribe("water/3");
-      client.subscribe("water/trans");
       client.subscribe("water/alarm");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
+      // Ждем 5 секунд и запускаем еще раз
       delay(5000);
     }
   }
 }
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(Relay1, OUTPUT);
   pinMode(Relay2, OUTPUT);
   pinMode(Relay3, OUTPUT);
   pinMode(Relay4, OUTPUT);
-  pinMode(Relay5, OUTPUT);
   pinMode(Woter1, OUTPUT);
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  digitalWrite(Relay1, HIGH); 
+  digitalWrite(Relay2, HIGH);
+  digitalWrite(Relay3, HIGH);
+  digitalWrite(Relay4, HIGH); 
 }
 
 void loop() {
@@ -165,11 +155,10 @@ void loop() {
   long now = millis();
 
   if (now - lastMsg > 10000) {
-    client.publish("water/major/check", String(digitalRead(Relay1)).c_str(), true);
-    client.publish("water/1/check",     String(digitalRead(Relay2)).c_str(), true);
-    client.publish("water/2/check",     String(digitalRead(Relay3)).c_str(), true);
-    client.publish("water/3/check",     String(digitalRead(Relay4)).c_str(), true);
-    client.publish("water/trans/check", String(digitalRead(Relay5)).c_str(), true);
+    client.publish("water/check/major", String(digitalRead(Relay1)).c_str(), true);
+    client.publish("water/check/1",     String(digitalRead(Relay2)).c_str(), true);
+    client.publish("water/check/2",     String(digitalRead(Relay3)).c_str(), true);
+    client.publish("water/check/3",     String(digitalRead(Relay4)).c_str(), true);
     client.publish("water/leakage",     String(digitalRead(Woter1)).c_str(), true);
   }
 }
